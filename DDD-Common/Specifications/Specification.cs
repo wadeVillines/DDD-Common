ee6223f942b26f3e2b72333385a9702d769d59c5
Wade Villines <wade.villines@safefoods.net>
2022-08-05 10:48:39 -0500

    update specifications

--- DDD-Common/Specifications/Specification.cs
+++ DDD-Common/Specifications/Specification.cs
@@ -4,10 +4,30 @@ namespace DDD.Common.Specifications
 {
     public abstract class Specification<T>
     {
-        public abstract Expression<Func<T, bool>> ToExpression();
+        public static readonly Specification<T> All = new IdentitySpecification<T>();
+
         public bool IsSatisfiedBy(T entity) => ToExpression().Compile()(entity);
-        public Specification<T> And(Specification<T> spec) => new AndSpecification<T>(this, spec);
-        public Specification<T> Or(Specification<T> spec) => new OrSpecification<T>(this, spec);
+
+        public abstract Expression<Func<T, bool>> ToExpression();
+
+        public Specification<T> And(Specification<T> spec)
+        {
+            if (this == All)
+                return spec;
+            if (spec == All)
+                return this;
+
+            return new AndSpecification<T>(this, spec);
+        }
+
+        public Specification<T> Or(Specification<T> spec)
+        {
+            if (this == All || spec == All)
+                return All;
+
+            return new OrSpecification<T>(this, spec);
+        }
+
         public Specification<T> Not(Specification<T> spec) => new NotSpecification<T>(spec);
     }
 }

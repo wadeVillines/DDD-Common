5598a2f10bf9adef99b70f84dc0e91eadabd3727
Wade Villines <wade.villines@safefoods.net>
2022-08-05 01:06:38 -0500

    update repository

---
+++ DDD-Common/Persistence/IPartitionedRepository.cs
@@ -0,0 +1,24 @@
+﻿using DDD.Common;
+using DDD.Common.Persistence;
+using DDD.Common.Specifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD_Common.Persistence
+{
+    public interface IPartitionedRepository<TEntity>
+        where TEntity : Entity<string>, IAggregateRoot
+    {
+        Task<List<TEntity>> ListAsync();
+        Task<List<TEntity>> ListAsync(string partitionKey);
+        Task<List<TEntity>> GetBySpecAsync(Specification<TEntity> spec);
+        Task<List<TEntity>> GetBySpecAsync(string partitionKey, Specification<TEntity> spec);
+        Task<TEntity> GetByIdAsync(string partitionKey, string id);
+        Task AddAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task RemoveAsync(string partitionKey, string id);
+    }
+}
--- DDD-Common/Persistence/IRepository.cs
+++ DDD-Common/Persistence/IRepository.cs
@@ -5,11 +5,11 @@ namespace DDD.Common.Persistence
     public interface IRepository<TEntity, TKey>
         where TEntity : Entity<TKey>, IAggregateRoot
     {
-        Task<List<TEntity>> List();
-        Task<List<TEntity>> GetBySpec(Specification<TEntity> spec);
-        Task<TEntity> GetById(TKey id);
-        Task Add(TEntity entity);
-        Task Update(TEntity entity);
-        Task Remove(TEntity entity);
+        Task<List<TEntity>> ListAsync();
+        Task<List<TEntity>> GetBySpecAsync(Specification<TEntity> spec);
+        Task<TEntity?> GetByIdAsync(TKey id);
+        Task AddAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task RemoveAsync(TEntity entity);
     }
 }
